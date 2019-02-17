196891bfb093f1bc7fa1e91063b2867852d02de2
Trung Truong <ttruong91498@gmail.com>
2019-02-16 22:20:42 -0500

    add blur effect

--- js/animation.js
+++ js/animation.js
@@ -1,81 +1,21 @@
-setTimeout(display, 500);
+for (let i = 0; i < document.images.length; i++) {
+  let image = document.images[i];
+  let bigImage = document.createElement("img");
 
-function display() {
-  document.querySelector("#loader").style.display = "none";
-  document.querySelector(".custom-container").style.display = "block";
-  window.sr = ScrollReveal();
-  sr.reveal("#about", {
-    duration: 1000,
-    origin: "top",
-    distance: "200px"
-  });
+  bigImage.onload = function() {
+    image.src = this.src;
 
-  sr.reveal("#myCarousel", {
-    duration: 500,
-    origin: "bottom"
-  });
+    document.querySelectorAll(".custom-caption").forEach(function(elem) {
+      elem.style.visibility = "visible";
+    });
 
-  sr.reveal("#me", {
-    duration: 500,
-    origin: "top"
-  });
+    document.querySelector("#myCarousel").className = "carousel slide no-blur";
+    image.className = "no-blur";
+  };
 
-  sr.reveal("#fuji", {
-    duration: 700,
-    origin: "bottom"
-  });
-  sr.reveal("#a-star", {
-    duration: 1000,
-    origin: "bottom"
-  });
-
-  sr.reveal("#nam", {
-    duration: 1000,
-    origin: "bottom"
-  });
-
-  sr.reveal("#robots", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#nam2", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#random", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#fern", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#blimp", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#tree", {
-    duration: 300,
-    origin: "bottom"
-  });
-
-  sr.reveal("#bridge", {
-    duration: 800,
-    origin: "top"
-  });
-
-  sr.reveal("#koch", {
-    duration: 500,
-    origin: "bottom"
-  });
-
-  sr.reveal("#triangle", {
-    duration: 500,
-    origin: "bottom"
-  });
+  setTimeout(function() {
+    let index = image.src.indexOf("min-");
+    let src = image.src.slice(index + 4);
+    bigImage.src = `assets/${src}`;
+  }, Math.random() * 500 + 500);
 }
